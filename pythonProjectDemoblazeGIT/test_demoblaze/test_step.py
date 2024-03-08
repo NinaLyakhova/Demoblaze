5a487d45abcfb56bf081d8dbcd06e4929d409328
NinaLyakhova <nina.lyakhova1985@yandex.ru>
2024-03-08 16:05:23 +0700

    test

--- pythonProjectDemoblazeGIT/test_demoblaze/test_step.py
+++ pythonProjectDemoblazeGIT/test_demoblaze/test_step.py
@@ -11,6 +11,6 @@ def login_form_demoblaze(browser, selenium_action): # pytest -k login test_step.
         selenium_action.action_click_element(locator_button_login_1)
         selenium_action.action_fill_input(locator_field_user_name, data_user_name)
         selenium_action.action_fill_input(locator_field_user_password, data_password)
-        selenium_action.action_click_element(locator_button_login_2)
+      #  selenium_action.action_click_element(locator_button_login_2)
     yield login_function
 
