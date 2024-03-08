985239017cc4bbd7d3e82a5dc25a0ca308970e10
NinaLyakhova <nina.lyakhova1985@yandex.ru>
2024-03-08 16:13:00 +0700

    test

--- pythonProjectDemoblazeGIT/test_demoblaze/locators.py
+++ pythonProjectDemoblazeGIT/test_demoblaze/locators.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 locator_button_login_1 = (By.XPATH, '//*[@id="login2"]')
-locator_button_login_2 = (By.XPATH, '/html/body/div[3]/div/div/div[3]/button[2]')
+locator_button_close_2 = (By.XPATH, '/html/body/div[3]/div/div/div[3]/button[1]')
 locator_field_user_name = (By.XPATH, '//*[@id="loginusername"]')
 locator_field_user_password = (By.XPATH, '//*[@id="loginpassword"]')
 
