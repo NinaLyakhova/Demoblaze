5e82dcfdfc336324fe487a9dec22372a636686ce
@NinaLyakhova <nina.lyakhova1985@yandex.ru>
2024-03-31 02:10:49 +0700

    test

--- pythonProjectDemoblazeGIT/test_demoblaze/test_check.py
+++ pythonProjectDemoblazeGIT/test_demoblaze/test_check.py
@@ -6,9 +6,10 @@ class TestDemoblaze:
     def test_sign_up_successful_demoblaze(self, browser, selenium_action, sign_up_form_demoblaze):
         """Регистрация пользователя"""
         sign_up_form_demoblaze()
-        # selenium_action.action_wait_on_page(1000)
+        selenium_action.action_wait_on_page(1000)
         assert "https://www.demoblaze.com/index.html" in browser.current_url.lower()
         print('Регистрация успешна!')
+
 
     @pytest.mark.smoke  # pytest -k test_login_successful test_demoblaze.test_check
     def test_login_successful_demoblaze(self, browser, selenium_action, login_form_demoblaze):
