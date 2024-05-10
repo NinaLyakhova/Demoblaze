b73eb6cd8eebd4a4e17b79c4f23cb92a5d4526bf
@NinaLyakhova <nina.lyakhova1985@yandex.ru>
2024-05-10 18:45:41 +0700

    PlayWright

--- pythonProjectPlaywright/Test_project_1/test_1.py
+++ pythonProjectPlaywright/Test_project_1/test_1.py
@@ -10,10 +10,12 @@ def test_example():
         browser.close()
 
 
-def test_XYZ():
+def test_bank_XYZ():
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
         page.goto("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
         assert page.inner_text('title') == 'XYZ Bank'
+        page.click('div.center:nth-child(1) > button:nth-child(1)')
+        page.screenshot(path='screen\example.png')
         browser.close()
